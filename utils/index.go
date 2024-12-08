5e29184661245b0fe7dedb28247d400f66533961
Pankaj Sha <pankajshadev@gmail.com>
2024-12-08 16:05:53 +0530

    Feat: added GetRandomUuid to generate random uuid string for auth state

--- utils/index.go
+++ utils/index.go
@@ -3,9 +3,15 @@ package utils
 import (
 	"encoding/json"
 	"net/http"
+
+	"github.com/google/uuid"
 )
 
 func WriteJson(w http.ResponseWriter, status int, data interface{}) error {
 	w.WriteHeader(status)
 	return json.NewEncoder(w).Encode(data)
+}
+
+func GetRandomUuid() string {
+	return uuid.NewString()
 }
